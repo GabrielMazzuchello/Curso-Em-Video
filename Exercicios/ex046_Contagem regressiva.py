5de832b0775b213cfd644890461475f87a87b788
Gabriel Mazzuchello Dal Molin <gabrieldalmolin02016@gmail.com>
2021-11-18 12:00:01 -0300

    Criando o programa do ex046

---
+++ Exercicios/ex046_Contagem regressiva.py
@@ -0,0 +1,7 @@
+from time import sleep
+print('Contagem regressiva para o lançamento dos fogos de artificios!!\n')
+for c in range(10, -1, -1):
+    print('Contagem regreciva: {}'.format(c))
+    sleep(1)
+print('\nEstourando os fogos de artificio...')
+print('BOOOM BOOM BOMM...')
