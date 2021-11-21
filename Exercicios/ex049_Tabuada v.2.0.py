2e408c7fb22d62d7ce0d2af63e0a4fca1f077b69
Gabriel Mazzuchello Dal Molin <gabrieldalmolin02016@gmail.com>
2021-11-21 20:55:48 -0300

    Create ex049_Tabuada v.2.0.py

---
+++ Exercicios/ex049_Tabuada v.2.0.py
@@ -0,0 +1,18 @@
+class cor:
+    BLACK = '\033[30m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GRAY = '\33[37m'
+    WHITE = '\033[97m'
+    CLEAR = '\033[m'
+
+
+numero = int(input('Informe o numero da tabuada: '))
+
+print('====={}Tabuada{}====='.format(cor.GREEN, cor.CLEAR))
+for c in range(1, 11):
+    print('{} X {} = {}{}{}'.format(numero, c, cor.RED, (numero * c), cor.CLEAR))
