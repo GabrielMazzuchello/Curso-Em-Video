fc137c23a1ccfd3154752d8f4990a5b3c30a8246
Gabriel Mazzuchello Dal Molin <gabrieldalmolin02016@gmail.com>
2022-05-09 18:00:27 -0300

    melhoria no ex058
    Jogo da Adivinhaçãov2.0.py

--- Exercicios/ex058_Jogo da Adivinhação v2.0.py
+++ Exercicios/ex058_Jogo da Adivinhação v2.0.py
@@ -1,5 +1,19 @@
 import random
 
+
+class cor:
+    BLACK = '\033[30m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GRAY = '\33[37m'
+    WHITE = '\033[97m'
+    CLEAR = '\033[m'
+
+
 computador = random.randint(1, 10)
 print('Acabei de pensar em um numero de 1 ate 10!')
 print('Quer tentar descobrir qual é?')
@@ -11,10 +25,14 @@ while not acertou:
     tentativa += 1
 
     if palpite == computador:
-        palpite = True
-        print('Parabens você acestou!, em {} tentativas'.format(tentativa))
-    elif palpite > computador:
-        print('Humm quase e MENOS!!')
-    elif palpite < computador:
-        print('Humm quase e MAIOR!!')
+        acertou = True
+        print('{}Parabens você acestou!, em {} tentativas{}'.format(cor.CYAN, tentativa, cor.CLEAR))
 
+    elif computador < palpite < 11:
+        print('{}Humm, quase e MENOS!!{}'.format(cor.RED, cor.CLEAR))
+
+    elif computador > palpite > -1:
+        print('{}Humm, quase e MAIOR!!{}'.format(cor.RED, cor.CLEAR))
+
+    elif palpite > 10:
+        print('{}OPS lembre-se os numeros são entre 0 e 10{}'.format(cor.YELLOW, cor.CLEAR))
