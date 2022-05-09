365cbc6e435c45ae1eb9097f20cfa0763df5e44f
Gabriel Mazzuchello Dal Molin <gabrieldalmolin02016@gmail.com>
2022-05-09 17:46:13 -0300

    criei o arquivo de cores

---
+++ Aulas/arquivo de cores.py
@@ -0,0 +1,12 @@
+class cor:
+
+    BLACK = '\033[30m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GRAY = '\33[37m'
+    WHITE = '\033[97m'
+    CLEAR = '\033[m'
